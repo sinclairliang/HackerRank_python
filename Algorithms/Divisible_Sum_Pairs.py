d8baf7adb7c1344f02ee59eb58f4d988f49e29a1
Sinclair Liang <sinclairliang@gmail.com>
2018-06-20 16:25:22 -0700

    made some changes, false interpretations

--- Algorithms/Divisible_Sum_Pairs.py
+++ Algorithms/Divisible_Sum_Pairs.py
@@ -11,12 +11,12 @@ import sys
 # Complete the divisibleSumPairs function below.
 def divisibleSumPairs(n, k, ar):
     number_of_pairs = 0
-    length = n
     for i in range(len(ar)):
-        for j in range(length):
-            if(ar[i]+ar[j] == k):
+        for j in range(i+1,len(ar)):
+            # print("i == "+ str(i)+" j == " + str(j))
+            if((ar[i]+ar[j])%k == 0):
+                # print("selected: i == "+ str(i)+" j == " + str(j))
                 number_of_pairs += 1
-        length -= 1
     return number_of_pairs
 
 if __name__ == '__main__':
